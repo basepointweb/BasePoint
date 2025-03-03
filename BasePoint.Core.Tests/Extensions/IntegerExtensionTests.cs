859a99bf10e16087804bf20da9ef0d7cb54ba761
basepointweb <basepointweb@gmail.com>
2025-03-03 15:39:22 -0300

    Create  SplitIntoBalancedParts extension method

---
+++ BasePoint.Core.Tests/Extensions/IntegerExtensionTests.cs
@@ -0,0 +1,52 @@
+﻿using BasePoint.Core.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace BasePoint.Core.Tests.Extensions
+{
+    public class IntegerExtensionTests
+    {
+        [Theory]
+        [InlineData(5, 3, "2,2,1")]
+        [InlineData(12, 4, "3,3,3,3")]
+        [InlineData(12, 5, "3,3,2,2,2")]
+        public void SplitIntoBalancedParts_PartSizeIsGreaterThanStringLength_ThrowsException(int inputInteger, int partsNumber, string expectedResultStr)
+        {
+            var expectedResultList = expectedResultStr.Split(',');
+
+            var expectedResult = new int[expectedResultList.Length];
+
+            var index = 0;
+
+            expectedResultList.ForEach(x =>
+            {
+                expectedResult[index] = int.Parse(expectedResultList[index]);
+                index++;
+            });
+
+            var parts = inputInteger.SplitIntoBalancedParts(partsNumber);
+            index = 0;
+
+            foreach (var part in parts)
+            {
+                part.Should().Be(expectedResult[index]);
+                index++;
+            }
+
+            parts.Sum().Should().Be(inputInteger);
+
+            parts.Should().HaveCount(expectedResultList.Length);
+        }
+
+        [Theory]
+        [InlineData(3, 5)]
+        [InlineData(1, 2)]
+        [InlineData(0, 1)]
+        public void SplitIntoBalancedParts_PartsNumberIsGreaterThanNumber_ThrowsException(int inputInteger, int partSize)
+        {
+            Action action = () => inputInteger.SplitIntoBalancedParts(partSize);
+
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+    }
+}
--- BasePoint.Core/Extensions/IntegerExtension.cs
+++ BasePoint.Core/Extensions/IntegerExtension.cs
@@ -18,5 +18,25 @@ namespace BasePoint.Core.Extensions
         {
             return (integerValue % 2 == Constants.Zero);
         }
+
+        public static List<int> SplitIntoBalancedParts(this int number, int parts)
+        {
+            ArgumentOutOfRangeException.ThrowIfLessThan(number, parts, "The number of parts must be less than or equals to number");
+
+            var result = new List<int>();
+
+            int baseValue = number / parts;
+            int remainder = number % parts;
+
+            for (int i = 0; i < parts; i++)
+            {
+                if (i < remainder)
+                    result.Add(baseValue + 1);
+                else
+                    result.Add(baseValue);
+            }
+
+            return result;
+        }
     }
 }
