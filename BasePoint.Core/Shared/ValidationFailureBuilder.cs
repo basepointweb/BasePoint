2554aa08ae6ec6ad3ad8102deac2087a4c291033
basepointweb <basepointweb@gmail.com>
2025-03-12 09:26:23 -0300

    Create ValidationFailureBuilder

---
+++ BasePoint.Core/Shared/ValidationFailureBuilder.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation.Results;
+
+namespace BasePoint.Core.Shared
+{
+    public class ValidationFailureBuilder
+    {
+        public ValidationFailure Build(string validationMessage)
+        {
+            return ConvertMessageToErrorMessage(validationMessage);
+        }
+
+        private static ValidationFailure ConvertMessageToErrorMessage(string message)
+        {
+            var code = Constants.ErrorCodes.DefaultErrorCode;
+            var errorMessage = message;
+
+            if (message.Contains(Constants.ErrorMessageSeparator))
+            {
+                var messageParts = message.Split(Constants.ErrorMessageSeparator);
+
+                code = messageParts[0];
+                errorMessage = messageParts[1];
+            }
+
+            return new ValidationFailure()
+            {
+                ErrorCode = code,
+                ErrorMessage = errorMessage
+            };
+        }
+    }
+}
