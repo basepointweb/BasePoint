fae75f27d7d485673977928a751b3d975d97487d
basepointweb <basepointweb@gmail.com>
2025-03-26 18:06:50 -0300

    Refactor command usecase error

--- BasePoint.Core.Cqrs.Dapper/Extensions/DynamicParametersExtension.cs
+++ BasePoint.Core.Cqrs.Dapper/Extensions/DynamicParametersExtension.cs
@@ -10,7 +10,7 @@ namespace BasePoint.Core.Cqrs.Dapper.Extensions
         public static void AddAsJson<T>(this DynamicParameters parameters, string parameterName, T parameterValue, JsonSerializerSettings settings = null) where T : class
         {
             if (parameterValue is null)
-                parameters.Add(parameterName, DBNull.Value);
+                parameters.Add(parameterName);
             else
             {
                 var jsonString = JsonConvert.SerializeObject(
@@ -24,7 +24,7 @@ namespace BasePoint.Core.Cqrs.Dapper.Extensions
         public static void AddAsJson<T>(this DynamicParameters parameters, string parameterName, T? parameterValue, JsonSerializerSettings settings = null) where T : struct
         {
             if (!parameterValue.HasValue)
-                parameters.Add(parameterName, DBNull.Value);
+                parameters.Add(parameterName);
             else
             {
                 var jsonString = JsonConvert.SerializeObject(
@@ -38,7 +38,7 @@ namespace BasePoint.Core.Cqrs.Dapper.Extensions
         public static void AddNullable<T>(this DynamicParameters parameters, string parameterName, T? parameterValue, int? size = null) where T : struct
         {
             if (!parameterValue.HasValue)
-                parameters.Add(parameterName, DBNull.Value, size: size);
+                parameters.Add(parameterName, size: size);
             else
                 parameters.Add(parameterName, parameterValue.Value, size: size);
         }
@@ -46,7 +46,7 @@ namespace BasePoint.Core.Cqrs.Dapper.Extensions
         public static void AddNullable(this DynamicParameters parameters, string parameterName, Guid? parameterValue, int? size = null)
         {
             if (!parameterValue.HasValue)
-                parameters.Add(parameterName, DBNull.Value, size: size);
+                parameters.Add(parameterName, size: size);
             else
                 parameters.Add(parameterName, parameterValue.Value, DbType.Guid, size: size);
         }
@@ -54,7 +54,7 @@ namespace BasePoint.Core.Cqrs.Dapper.Extensions
         public static void AddNullable(this DynamicParameters parameters, string parameterName, string parameterValue, int? size = null)
         {
             if (parameterValue is null)
-                parameters.Add(parameterName, DBNull.Value, DbType.AnsiString, size: size);
+                parameters.Add(parameterName, DbType.AnsiString, size: size);
             else
                 parameters.Add(parameterName, parameterValue, DbType.AnsiString, size: size);
         }
@@ -62,7 +62,7 @@ namespace BasePoint.Core.Cqrs.Dapper.Extensions
         public static void AddNullable(this DynamicParameters parameters, string parameterName, DateTime? parameterValue, int? size = null)
         {
             if (!parameterValue.HasValue)
-                parameters.Add(parameterName, DBNull.Value, DbType.DateTime2, size: size);
+                parameters.Add(parameterName, DbType.DateTime2, size: size);
             else
                 parameters.Add(parameterName, parameterValue, DbType.DateTime2, size: size);
         }
@@ -70,7 +70,7 @@ namespace BasePoint.Core.Cqrs.Dapper.Extensions
         public static void AddNullable(this DynamicParameters parameters, string parameterName, DateTimeOffset? parameterValue, int? size = null)
         {
             if (!parameterValue.HasValue)
-                parameters.Add(parameterName, DBNull.Value, DbType.DateTimeOffset, size: size);
+                parameters.Add(parameterName, DbType.DateTimeOffset, size: size);
             else
                 parameters.Add(parameterName, parameterValue, DbType.DateTimeOffset, size: size);
         }
@@ -82,7 +82,7 @@ namespace BasePoint.Core.Cqrs.Dapper.Extensions
             else
             {
                 if (parameterValue is null)
-                    parameters.Add(parameterName, DBNull.Value, size: size);
+                    parameters.Add(parameterName, size: size);
                 else
                 {
                     var parameterType = parameterValue.GetType();
@@ -98,7 +98,7 @@ namespace BasePoint.Core.Cqrs.Dapper.Extensions
         public static void AddNullable(this DynamicParameters parameters, string parameterName, IBaseEntity parameterValue, int? size = null)
         {
             if (parameterValue is null)
-                parameters.Add(parameterName, DBNull.Value, size: size);
+                parameters.Add(parameterName, size: size);
             else
                 parameters.Add(parameterName, parameterValue.Id, DbType.Guid, size: size);
         }
